4afebf70a50e86347fd7bae41a2acc026a7b7cbb
davidGarcia <djgj57@hotmail.com>
2020-12-18 20:05:11 -0500

    PasandoPruebasSemana5

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -32,7 +32,7 @@ exports.login = async(req, res, next) => {
     }
 };
 
-exports.register = async(req, res, next) =>{
+exports.add = async(req, res, next) =>{
     try{
         const user = await models.Usuario.findOne({ where: { email: req.body.email}});
         if(user){
@@ -40,7 +40,7 @@ exports.register = async(req, res, next) =>{
                 message: 'Sorry your request has a conflict with our system state, maybe the email is already in use'
             })
         }else{
-            req.body.password = bcrypt.hashSync(req.body.password, 10);
+            req.body.password = bcrypt.hashSync(req.body.password, 8);
             const user = await models.Usuario.create(req.body);
             res.status(200).json(user)
         }
@@ -74,8 +74,8 @@ exports.update = async(req, res, next) =>{
     try{
         const user = await models.Usuario.findOne({ where: { email: req.body.email}});
         if(user){
-            const user = await models.Usuario.update({name: req.body.name},
-                {where: {email: req.body.email},
+            const user = await models.Usuario.update({rol: req.body.rol, nombre: req.body.nombre, email: req.body.email},
+                {where: {id: req.body.id},
                 });
                 res.status(200).json(user);
             }else{
@@ -90,3 +90,37 @@ exports.update = async(req, res, next) =>{
             next(error);
         }
     };   
+
+exports.activate = async(req, res, next) =>{
+    try{
+        const register = await models.Usuario.update({estado: 1},
+                {
+                    where: {
+                        id: req.body.id
+                    },
+                });
+                res.status(200).json(register);
+        }catch(error) {
+            res.status(500).send({
+                message: 'Error.'
+            });
+            next(error);
+        }
+    };       
+
+exports.deactivate = async(req, res, next) =>{
+    try{
+        const register = await models.Usuario.update({estado: 0},
+                {
+                    where: {
+                        id: req.body.id
+                    },
+                });
+                res.status(200).json(register);
+        }catch(error) {
+            res.status(500).send({
+                message: 'Error.'
+            });
+            next(error);
+        }
+    };    
--- routes/api/usuario.js
+++ routes/api/usuario.js
@@ -3,8 +3,11 @@ const usuarioController = require('../../controllers/UsuarioController');
 const auth = require('../../middlewares/auth')
 
 router.get('/list', auth.verificarAdministrador, usuarioController.list);
-router.post('/register', auth.verificarAdministrador, usuarioController.register);
+router.post('/add', auth.verificarAdministrador, usuarioController.add);
 router.put('/update', auth.verificarAdministrador, usuarioController.update);
 router.post('/login', usuarioController.login);
+
+router.put('/activate', auth.verificarAdministrador, usuarioController.activate);
+router.put('/deactivate', auth.verificarAdministrador, usuarioController.deactivate);
 
 module.exports = router;
