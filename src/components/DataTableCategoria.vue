556affd04f844c0041cf59a2afcc2f8d20812c1c
davidGarcia <djgj57@hotmail.com>
2020-12-21 09:03:30 -0500

    Upgrades

--- src/components/DataTableCategoria.vue
+++ src/components/DataTableCategoria.vue
@@ -88,11 +88,11 @@
           </v-dialog>
           <v-dialog v-model="dialogDelete" max-width="500px">
             <v-card>
-              <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+              <v-card-title class="headline">¿Está seguro de cambiar el estado?</v-card-title>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                <v-btn color="blue darken-1" text @click="closeDelete">Cancelar</v-btn>
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm">Si</v-btn>
                 <v-spacer></v-spacer>
               </v-card-actions>
             </v-card>
