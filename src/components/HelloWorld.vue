11db7aa8b63a32559d1dcd9d0460fd7544480992
davidGarcia <djgj57@hotmail.com>
2020-12-21 20:06:43 -0500

    card color update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -38,7 +38,7 @@
   <v-card
     v-for="articulo in articulos" :key="articulo.id"
     class="mx-auto"
-    color="#26c6da"
+    color="#BA9BCF"
     dark
     max-width="300"
   >
@@ -58,32 +58,32 @@
 
     <v-card-actions>
       <v-list-item class="grow">
-        <v-list-item-avatar color="grey darken-3">
+        <!-- <v-list-item-avatar color="grey darken-3">
           <v-img
             class="elevation-6"
             alt=""
             src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
           ></v-img>
-        </v-list-item-avatar>
+        </v-list-item-avatar> -->
 
         <v-list-item-content>
-          <v-list-item-title>Evan You</v-list-item-title>
+          <v-list-item>Producto disponible para {{articulo.categoria.nombre}}</v-list-item>
         </v-list-item-content>
 
-        <v-row
+        <!-- <v-row 
           align="center"
-          justify="end"
-        >
-          <v-icon class="mr-1">
+          justify="end" -->
+        <!-- > -->
+          <!-- <v-icon class="mr-1">
             mdi-heart
-          </v-icon>
-          <span class="subheading mr-2">256</span>
-          <span class="mr-1">·</span>
-          <v-icon class="mr-1">
+          </v-icon> -->
+          <!-- <span class="subheading mr-2">256</span> -->
+          <!-- <span class="mr-1">·</span> -->
+          <!-- <v-icon class="mr-1">
             mdi-share-variant
-          </v-icon>
-          <span class="subheading">45</span>
-        </v-row>
+          </v-icon> -->
+          <!-- <span class="subheading">45</span> -->
+        <!-- </v-row> -->
       </v-list-item>
     </v-card-actions>
   </v-card>  
@@ -123,13 +123,7 @@
       </v-card-text>
     </v-card>
   </v-footer>
-
-<pre>
-  {{articulos}}
-</pre>
-
-
-    </div>
+   </div>
 </template>
 
 
